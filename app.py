0392358054c990bee5fda87baa71ec00567ae279
Jason <bilafish@gmail.com>
2020-04-27 13:57:28 +0800

    :card_file_box: Initialise migration for Flask-Migrate

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ import logging
 from logging import Formatter, FileHandler
 from flask_wtf import Form
 from forms import *
+from flask_migrate import Migrate
 
 # ----------------------------------------------------------------------------#
 # App Config.
@@ -22,6 +23,7 @@ moment = Moment(app)
 app.config.from_object("config")
 db = SQLAlchemy(app)
 
+migrate = Migrate(app, db)
 
 # ----------------------------------------------------------------------------#
 # Models.
