30f41ca6c21c7d87d648aab114aafa5d958aba16
Jason <bilafish@gmail.com>
2020-05-01 22:44:04 +0800

    :sparkles: Add shows data to show_venue route

--- app.py
+++ app.py
@@ -134,6 +134,22 @@ app.jinja_env.filters["datetime"] = format_datetime
 # ----------------------------------------------------------------------------#
 
 
+def sortUpcomingShows(shows, now):
+    sortedShows = {"past": [], "upcoming": []}
+    for show in shows:
+        showDict = {
+            "artist_id": show.artist.id,
+            "artist_name": show.artist.name,
+            "artist_image_link": show.artist.image_link,
+            "start_time": show.start_time.strftime("%Y-%m-%dT%H:%M:%S.%fZ"),
+        }
+        if show.start_time > now:
+            sortedShows["upcoming"].append(showDict)
+        else:
+            sortedShows["past"].append(showDict)
+    return sortedShows
+
+
 def countIsUpcoming(shows, now):
     result = 0
     for show in shows:
@@ -214,7 +230,16 @@ def search_venues():
 def show_venue(venue_id):
     # shows the venue page with the given venue_id
     # TODO: Add query to get shows data
-    dbData = Venue.query.get(venue_id)
+    dbData = (
+        Venue.query.join(Show, isouter=True)
+        .join(Artist, isouter=True)
+        .filter(Venue.id == venue_id)
+        .first()
+    )
+    now = datetime.now()
+    # Sort shows based on start_time into whether upcoming or past
+    sortedShows = sortUpcomingShows(dbData.shows, now)
+
     # Transform data into format for view to render
     parsedData = {
         "id": dbData.id,
@@ -229,103 +254,12 @@ def show_venue(venue_id):
         "seeking_talent": dbData.seeking_talent,
         "seeking_description": dbData.seeking_description,
         "image_link": dbData.image_link,
-        "past_shows": [
-            {
-                "artist_id": 4,
-                "artist_name": "Guns N Petals",
-                "artist_image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
-                "start_time": "2019-05-21T21:30:00.000Z",
-            }
-        ],
-        "upcoming_shows": [],
-        "past_shows_count": 1,
-        "upcoming_shows_count": 0,
+        "past_shows": sortedShows["past"],
+        "upcoming_shows": sortedShows["upcoming"],
+        "past_shows_count": len(sortedShows["past"]),
+        "upcoming_shows_count": len(sortedShows["upcoming"]),
     }
-    # data1 = {
-    #     "id": 1,
-    #     "name": "The Musical Hop",
-    #     "genres": ["Jazz", "Reggae", "Swing", "Classical", "Folk"],
-    #     "address": "1015 Folsom Street",
-    #     "city": "San Francisco",
-    #     "state": "CA",
-    #     "phone": "123-123-1234",
-    #     "website": "https://www.themusicalhop.com",
-    #     "facebook_link": "https://www.facebook.com/TheMusicalHop",
-    #     "seeking_talent": True,
-    #     "seeking_description": "We are on the lookout for a local artist to play every two weeks. Please call us.",
-    #     "image_link": "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
-    #     "past_shows": [
-    #         {
-    #             "artist_id": 4,
-    #             "artist_name": "Guns N Petals",
-    #             "artist_image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
-    #             "start_time": "2019-05-21T21:30:00.000Z",
-    #         }
-    #     ],
-    #     "upcoming_shows": [],
-    #     "past_shows_count": 1,
-    #     "upcoming_shows_count": 0,
-    # }
-    # data2 = {
-    #     "id": 2,
-    #     "name": "The Dueling Pianos Bar",
-    #     "genres": ["Classical", "R&B", "Hip-Hop"],
-    #     "address": "335 Delancey Street",
-    #     "city": "New York",
-    #     "state": "NY",
-    #     "phone": "914-003-1132",
-    #     "website": "https://www.theduelingpianos.com",
-    #     "facebook_link": "https://www.facebook.com/theduelingpianos",
-    #     "seeking_talent": False,
-    #     "image_link": "https://images.unsplash.com/photo-1497032205916-ac775f0649ae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
-    #     "past_shows": [],
-    #     "upcoming_shows": [],
-    #     "past_shows_count": 0,
-    #     "upcoming_shows_count": 0,
-    # }
-    # data3 = {
-    #     "id": 3,
-    #     "name": "Park Square Live Music & Coffee",
-    #     "genres": ["Rock n Roll", "Jazz", "Classical", "Folk"],
-    #     "address": "34 Whiskey Moore Ave",
-    #     "city": "San Francisco",
-    #     "state": "CA",
-    #     "phone": "415-000-1234",
-    #     "website": "https://www.parksquarelivemusicandcoffee.com",
-    #     "facebook_link": "https://www.facebook.com/ParkSquareLiveMusicAndCoffee",
-    #     "seeking_talent": False,
-    #     "image_link": "https://images.unsplash.com/photo-1485686531765-ba63b07845a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=747&q=80",
-    #     "past_shows": [
-    #         {
-    #             "artist_id": 5,
-    #             "artist_name": "Matt Quevedo",
-    #             "artist_image_link": "https://images.unsplash.com/photo-1495223153807-b916f75de8c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
-    #             "start_time": "2019-06-15T23:00:00.000Z",
-    #         }
-    #     ],
-    #     "upcoming_shows": [
-    #         {
-    #             "artist_id": 6,
-    #             "artist_name": "The Wild Sax Band",
-    #             "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-    #             "start_time": "2035-04-01T20:00:00.000Z",
-    #         },
-    #         {
-    #             "artist_id": 6,
-    #             "artist_name": "The Wild Sax Band",
-    #             "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-    #             "start_time": "2035-04-08T20:00:00.000Z",
-    #         },
-    #         {
-    #             "artist_id": 6,
-    #             "artist_name": "The Wild Sax Band",
-    #             "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-    #             "start_time": "2035-04-15T20:00:00.000Z",
-    #         },
-    #     ],
-    #     "past_shows_count": 1,
-    #     "upcoming_shows_count": 1,
-    # }
+
     return render_template("pages/show_venue.html", venue=parsedData)
 
 
